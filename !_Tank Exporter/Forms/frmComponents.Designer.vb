63a6ece965f4731db937d30d6317dc24f789cee0
kresneck <63115719+kresneck@users.noreply.github.com>
2022-04-14 21:02:22 -0500

    Update form size, component layout, and tab order.

--- !_Tank Exporter/Forms/frmComponents.Designer.vb
+++ !_Tank Exporter/Forms/frmComponents.Designer.vb
@@ -39,27 +39,27 @@ Partial Class frmComponents
         Me.tv_guns.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.tv_guns.CheckBoxes = True
         Me.tv_guns.ForeColor = System.Drawing.Color.White
-        Me.tv_guns.Location = New System.Drawing.Point(12, 24)
+        Me.tv_guns.Location = New System.Drawing.Point(12, 166)
         Me.tv_guns.Name = "tv_guns"
-        Me.tv_guns.Size = New System.Drawing.Size(136, 115)
-        Me.tv_guns.TabIndex = 0
+        Me.tv_guns.Size = New System.Drawing.Size(190, 115)
+        Me.tv_guns.TabIndex = 5
         '
         'tv_turrets
         '
         Me.tv_turrets.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.tv_turrets.CheckBoxes = True
         Me.tv_turrets.ForeColor = System.Drawing.Color.White
-        Me.tv_turrets.Location = New System.Drawing.Point(154, 24)
+        Me.tv_turrets.Location = New System.Drawing.Point(208, 166)
         Me.tv_turrets.Name = "tv_turrets"
-        Me.tv_turrets.Size = New System.Drawing.Size(136, 115)
-        Me.tv_turrets.TabIndex = 1
+        Me.tv_turrets.Size = New System.Drawing.Size(190, 115)
+        Me.tv_turrets.TabIndex = 7
         '
         'continue_bt
         '
-        Me.continue_bt.Location = New System.Drawing.Point(116, 233)
+        Me.continue_bt.Location = New System.Drawing.Point(168, 288)
         Me.continue_bt.Name = "continue_bt"
         Me.continue_bt.Size = New System.Drawing.Size(75, 23)
-        Me.continue_bt.TabIndex = 2
+        Me.continue_bt.TabIndex = 8
         Me.continue_bt.Text = "Continue"
         Me.continue_bt.UseVisualStyleBackColor = True
         '
@@ -67,17 +67,17 @@ Partial Class frmComponents
         '
         Me.Label1.AutoSize = True
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(154, 8)
+        Me.Label1.Location = New System.Drawing.Point(208, 150)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(73, 13)
-        Me.Label1.TabIndex = 3
+        Me.Label1.TabIndex = 6
         Me.Label1.Text = "Found Turrets"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(12, 8)
+        Me.Label2.Location = New System.Drawing.Point(12, 150)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(65, 13)
         Me.Label2.TabIndex = 4
@@ -87,10 +87,10 @@ Partial Class frmComponents
         '
         Me.Label3.AutoSize = True
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(12, 142)
+        Me.Label3.Location = New System.Drawing.Point(12, 12)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(63, 13)
-        Me.Label3.TabIndex = 7
+        Me.Label3.TabIndex = 0
         Me.Label3.Text = "Found Hulls"
         '
         'tv_hulls
@@ -98,19 +98,19 @@ Partial Class frmComponents
         Me.tv_hulls.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.tv_hulls.CheckBoxes = True
         Me.tv_hulls.ForeColor = System.Drawing.Color.White
-        Me.tv_hulls.Location = New System.Drawing.Point(12, 158)
+        Me.tv_hulls.Location = New System.Drawing.Point(12, 28)
         Me.tv_hulls.Name = "tv_hulls"
-        Me.tv_hulls.Size = New System.Drawing.Size(136, 69)
-        Me.tv_hulls.TabIndex = 6
+        Me.tv_hulls.Size = New System.Drawing.Size(190, 115)
+        Me.tv_hulls.TabIndex = 1
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(154, 142)
+        Me.Label4.Location = New System.Drawing.Point(208, 12)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(76, 13)
-        Me.Label4.TabIndex = 9
+        Me.Label4.TabIndex = 2
         Me.Label4.Text = "Found Chassis"
         '
         'tv_chassis
@@ -118,17 +118,17 @@ Partial Class frmComponents
         Me.tv_chassis.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.tv_chassis.CheckBoxes = True
         Me.tv_chassis.ForeColor = System.Drawing.Color.White
-        Me.tv_chassis.Location = New System.Drawing.Point(154, 158)
+        Me.tv_chassis.Location = New System.Drawing.Point(208, 28)
         Me.tv_chassis.Name = "tv_chassis"
-        Me.tv_chassis.Size = New System.Drawing.Size(136, 69)
-        Me.tv_chassis.TabIndex = 8
+        Me.tv_chassis.Size = New System.Drawing.Size(190, 115)
+        Me.tv_chassis.TabIndex = 3
         '
         'frmComponents
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(302, 266)
+        Me.ClientSize = New System.Drawing.Size(410, 323)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.tv_chassis)
         Me.Controls.Add(Me.Label3)
